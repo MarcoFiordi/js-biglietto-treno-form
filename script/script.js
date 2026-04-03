41184f40db2b0849e695ff27b74d62a726a3bbb8
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-03 17:46:18 +0200

    dichiarata variabile prezzo per km e selezionati gli elementi DOM per gestione form

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
-'use strict'
+'use strict';
+
+const form = document.querySelector('#ticket-form');
+const kmInput = document.querySelector('#km');
+const ageSelect = document.querySelector('#age-range');
+const resultElement = document.querySelector('#result');
+
+const pricePerKm = 0.21;
+
